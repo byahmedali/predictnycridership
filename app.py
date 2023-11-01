2f441f4266d3c2d67dd0db47692b60278d8701d2
byahmedali <96247069+byahmedali@users.noreply.github.com>
2023-11-01 14:46:29 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ The inputs required for this prediction are:
 - The Trip Origin Borough 
 - The Date for the Prediction
 - The Ride Type 
+
 This prediction model enables taxi and for-hire-vehicle dispatching services with an estimate of the daily number of trips originating at a given borough in NYC on any given day. The City of New York can use this information to better manage its fleet and more accurately meet demand. This helps them reduce wait times, have a lower number of idle vehicles, less wasted resources, and mitigate their negative contribution to traffic and the environment. 
 
 You can try the prediction model below yourself.
@@ -24,14 +25,14 @@ You can try the prediction model below yourself.
 st.write("You can adjust some parameters according to your need, ")
 pickup_location = st.selectbox("Pickup Borough", ["Manhattan","Bronx", "Brooklyn", "EWR",  "Queens", "Staten Island" ])
 
-selected_date = st.date_input("Select a date from 2023 onwards", date(2023, 1, 1))
+selected_date = st.date_input("Select a date", date(2023, 1, 1))
 year = selected_date.year
 month = int(selected_date.month)
 day = selected_date.day
 
 
 selected_day = selected_date.strftime("%A")[0:3]
-st.write(f"You selected {selected_day} in week")
+#st.write(f"You selected {selected_day} in week")
 if day  in ["Monday","Tuesday","Wednesday","Thursday","Friday"]:
   workday=1
 else:
@@ -40,13 +41,13 @@ else:
 # season_dict={ [['12','1','2']]:"Winter",[['3','4','5']]:"Spring",[['6','7','8']]:"Summer",[['9','10','11']]:"Autumn"}
 # season_dict={ ['12','1','2']:"Winter",['3','4','5']:"Spring",['6','7','8']:"Summer",['9','10','11']:"Autumn"}
 # season_dict={ "Winter":['12','1','2'],"Spring":['3','4','5'],"Summer":['6','7','8'],"Autumn":['9','10','11']}
-if month>2 and month <6:
+if month>=1 and month <3 and month=12:
     season="Winter"
-if month>5 and month <9:
+elif month>=3 and month <6:
   season="Spring"
-if month>8 and month <11:
+elif month>=6 and month <9:
     season="Summer"
-else:
+elif month>=9 and month<12:
     season="Autumn"
 # for month_index in season_dict.keys():
     # print(month)
@@ -54,7 +55,7 @@ else:
         # print(season_dict.get())
         # print("hkjhkhkjh")
 # season = st.selectbox("Select a season", ["Spring", "Summer", "Fall", "Winter"])
-taxi_type = st.selectbox("Select a taxi", ["GREEN Taxi", "Yellow Taxi", "High Volume For-Hire Vehicle"])
+taxi_type = st.selectbox("Ride type", ["Green Taxi", "Yellow Taxi", "High Volume For-Hire Vehicle"])
 
 #  -------------------------------------
 
@@ -79,8 +80,6 @@ data =  {
         "Month": month, # 1 - 12
         "Day": day, # 1 - 31
         "Season": season, #Spring, Summer, Autumn, Winter
-  
-        # "PublicHoliday": 0, # 1 (Yes), 0 (No)
         "DayOfWeek": selected_day, #Sun, Mon, Tue, Wed, Thu, Fri, Sat
         "Workday": workday # 1 (Yes), 0 (No)
       }
@@ -97,14 +96,14 @@ url = 'http://ff8b8ccf-0dfb-48f0-b6f3-5e7954b6d1c6.eastus.azurecontainer.io/scor
 
 headers = {'Content-Type':'application/json'}
 req = urllib.request.Request(url, body, headers)
-if st.button("Predict value"):
+if st.button("Predict"):
     try:
         response = urllib.request.urlopen(req)
 
         result = json.loads(response.read())
         st.write("Predicted Ride Count:" )
         st.title(int(round(result["Results"][0])))
-        st.write(f"At  {year}-{month}-{day} ")
+        st.write(f"On  {year}-{month}-{day} ")
 
         # st.write(f"The ride count is {int(round(result['Results'][0],3))} \t At:>  DATE: {year}-{month}-{day}")
     except urllib.error.HTTPError as error:
